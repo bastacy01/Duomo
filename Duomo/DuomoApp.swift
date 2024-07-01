d12d9a041140482b8c19e550203964f8167b607b
Ben Stacy <ben.stacy98@outlook.com>
2024-07-01 03:56:46 -0400

    Initial Commit

---
+++ Duomo/DuomoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DuomoApp.swift
+//  Duomo
+//
+//  Created by Ben Stacy on 7/1/24.
+//
+
+import SwiftUI
+
+@main
+struct DuomoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
